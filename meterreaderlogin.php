5c6a1ca3477f72b60e879985185086734fb0019f
Susinidu Sadeepa <susinidusadeepa@gmail.com>
2023-01-06 17:32:43 +0530

    Few changes in meter reader login

--- meterreaderlogin.php
+++ meterreaderlogin.php
@@ -1,16 +1,16 @@
 <?php
      if(($_POST['username'] == "001") && ($_POST['pass'] == "bentram37"))
   { 
-    
+    echo "login...";
   }
  elseif(($_POST['username'] == "002") && ($_POST['pass'] == "blackroad19"))
  {
-    
+    echo "login...";
 }
     elseif(($_POST['username'] == "003") && ($_POST['pass'] == "murkypaste20"))
     {
         
-        
+        echo "login...";
     }
     else{ ?>
 
