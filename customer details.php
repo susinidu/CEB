3cd7719fbe48b8b82af67acc6219c6320497ae8b
Susinidu Sadeepa <susinidusadeepa@gmail.com>
2023-01-06 09:56:40 +0530

    create form for add details

--- customer details.php
+++ customer details.php
@@ -4,9 +4,22 @@
 ?>
 
 <html>
+    
 
 <h2>Customer details</h2>
 
-    <form action 
+    <form action="" method ="post">
+        <div class="form group">
+            <label>Customer Account Number :</label>
+            <dd><input type ="number" name="customeraccountnumber" id="customeraccountnumber" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>Date :</label>
+            <dd><input type ="date" name="date" id="date" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>Meter Reading Value :</label>
+            <dd><input type ="number" name="mrv" id="mrv" class="form control"></dd>
+        </div>
 
     </html>
